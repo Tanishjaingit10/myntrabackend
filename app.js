f93757661703367f05ec3a313800d6f3399ae8a7
Tanishjaingit10 <Tanish.jain.961@gmail.com>
2021-10-26 16:32:56 +0530

    Google Authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./keys')
+const cors = require('cors');
 // 064NsPjMmbIkDuNj
 
 mongoose.connect(MONGOURI)
@@ -16,6 +17,10 @@ mongoose.connection.on('error',(err) => {
 
 require('./models/user')
 require('./models/post')
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(express.json())
 app.use(require('./routes/auth'))
